0f36e40e9b9ecf35a3e36330064625e2d5a5fbe1
James McDaniel <icathaid@gmail.com>
2018-04-16 15:04:51 -0700

    testing cors issue

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,8 +1,36 @@
 const ENV = {};
+const express = require('express');
+const pg = require('pg');
+// const app = express();
+const cors = require('cors');
 
 ENV.isProduction = window.location.protocol === 'https';
 ENV.productionApiUrl = 'https://james-booklist.herokuapp.com';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
-(function pol(){console.log('james is a funky cat')});
+(function(module) {
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+  function Book(bookObject){
+    Object.keys(bookObject).forEach(key => this[key] = bookObject[key]);
+  }
+  Book.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#task-template').text());
+    return template(this);
+  }
+  Book.all = [];
+  Book.LoadAll = rows => {
+    Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
+  }
+  Book.fetchAll = callback => 
+    $.get(`${ENV.apiUrl}/Books`)
+      .then(Book.loadAll)
+      .then(callback)
+      .catch(errorCallback);
+
+  module.Book = Book;
+})(app);
+app.use(cors());
